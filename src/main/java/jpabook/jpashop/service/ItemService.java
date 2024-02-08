b0be3f3c66a2dffb24e54e14f1fdcd1a38b039a3
dnfheh <dnfheh@naver.com>
2024-02-08 18:44:10 +0900

    20240208

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -19,7 +19,13 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
-
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
