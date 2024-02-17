6461f3fd1f6d553342e3707c1bc1e43255650aa6
dnfheh <dnfheh@naver.com>
2024-02-17 14:37:09 +0900

    20240217

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,8 +20,8 @@ public class Member {
     @Embedded
     private Address address;
 
-    @OneToMany
-    @JoinColumn(name = "member_id")
+    @JsonIgnore
+    @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
 
